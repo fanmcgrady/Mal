449c730e777a2b88d7edc0d8c2ed258baaca7c9f
zyzshishui <492129152@qq.com>
2022-08-11 10:44:35 +0800

    Changes

---
+++ config.py
@@ -0,0 +1,8 @@
+CLAMAV_PATH = "E://AppData//ClamAV//clamd.conf"
+# CLAMAV_PATH = "D:\\engines\\ClamAV\\clamav-0.105.0-rc.win.x64"
+# SAMPLE_PATH = r"D:\samples\notepad.exe"
+# CLAMAV_PATH = "Z:/home/zyzshishui/ClamAV"
+TEST_SAMPLE_PATH = r"C:\Windows\System32\notepad.exe"
+MAL_SAMPLE_PATH = r"E:\AppData\Endermanch@Ana.exe"
+TEST_SIZE = 1
+SAMPLE_PATH = "./gym_malware/envs/utils/samples"
