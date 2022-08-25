85d275850d2dd9c6c2060ffc79ead298577d6f28
fangzhiyang <fanmcgrady@gmail.com>
2022-08-25 16:54:30 +0800

    重新梳理state

--- pre_filter.py
+++ pre_filter.py
@@ -1,16 +1,15 @@
-import os
-import sys
 from config import *
+from tqdm import tqdm
 from gym_malware.envs.utils.reward import ClamAV
 module_path = os.path.split(os.path.abspath(sys.modules[__name__].__file__))[0]
 sample_dir_path = module_path + r"\gym_malware\envs\utils\samples"
 ca = ClamAV()
 benign = 0
-for sample in os.listdir(sample_dir_path):
+for sample in tqdm(os.listdir(sample_dir_path)):
     sample_path = os.path.join(sample_dir_path, sample)
     if ca.scan(sample_path) == 0:
         benign += 1
         os.remove(sample_path)
-        with open(LOG_PATH+r"\preprocess_log.txt", "a+") as f:
+        with open(LOG_PATH + r"\preprocess_log.txt", "a+") as f:
             f.write("{}: delete {}\n".format(benign, sample_path))
 print(benign)
