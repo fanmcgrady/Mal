b49d97f26b344a4dd01f43428efc30b663596053
fangzhiyang <fanmcgrady@gmail.com>
2022-08-26 21:24:17 +0800

    修改log路径，保证在Linux上可用

--- config.py
+++ config.py
@@ -4,6 +4,6 @@ import sys
 ROOT_PATH = os.path.split(os.path.abspath(sys.modules[__name__].__file__))[0]
 TEST_SAMPLE_PATH = r"C:\Windows\System32\notepad.exe"
 MAL_SAMPLE_PATH = r"D:\code\Mal\gym_malware\envs\utils\samples\DoS.Win32.ARPKiller.13"
-LOG_PATH = ROOT_PATH + r"\logs"
+LOG_PATH = ROOT_PATH + r"\\logs"
 SAMPLE_PATH = ROOT_PATH + r"\gym_malware\envs\utils\samples"
 TEST_SIZE = 1
