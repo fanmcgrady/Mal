22d1381fabd21c50a4a11838cd5608dd0887d493
fangzhiyang <fanmcgrady@gmail.com>
2022-08-27 22:01:36 +0800

    '修改pre'

--- pre_filter.py
+++ pre_filter.py
@@ -1,17 +1,16 @@
 import os
 from config import *
+from gym_malware.envs.utils import interface
 
 from gym_malware.envs.utils.reward import ClamAV
 
-module_path = os.path.split(os.path.abspath(sys.modules[__name__].__file__))[0]
-sample_dir_path = os.path.join(module_path, "gym_malware", "envs", "utils", "samples")
 ca = ClamAV()
 benign = 0
-for sample in os.listdir(sample_dir_path):
-    sample_path = os.path.join(sample_dir_path, sample)
-    if ca.scan(sample_path) == 0:
+samples = interface.get_samples()
+for sample in samples:
+    if ca.scan(sample) == 0:
         benign += 1
-        os.remove(sample_path)
+        os.remove(sample)
         with open(os.path.join(LOG_PATH, "preprocess_log.txt"), "a+") as f:
-            f.write("{}: delete {}\n".format(benign, sample_path))
+            f.write("{}: delete {}\n".format(benign, sample))
 print(benign)
